47892f7709f87c0f006ee906574c1fd2ba27ae00
MarinaCotta <marinacotta@id.uff.br>
2021-01-10 17:07:51 -0300

    GET/orders

--- backend/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
@@ -1,10 +1,18 @@
 package com.devsuperior.dsdeliver.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsdeliver.entities.Order;
 
 //TRAZ IMPLEMENTACOES PARA BUSCAR, SALVAR, DELETAR E ATUALIZAR DADOS COFORME A ENTIDADE QUE PRECISA(ORDER, TIPO DA CHAVE)
 public interface OrderRepository extends JpaRepository<Order,Long>{
 
+	// método para retornar só os pediso pendentes, ordenado por moment
+	@Query("SELeCT DISTINCT obj FROM Order obj JOIN FETCH obj.products "+
+	"WHERE obj.status = 0 ORDER BY obj.moment ASC")
+	List <Order> findOrdersWithProducts ();
+	
 }
